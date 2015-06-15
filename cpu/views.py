51bf4a4cba08f1595d638786ff80669a5670e41b
Fanyu Kong <njukongfy@gmail.com>
2015-06-15 14:48:19 +0800

    calc pi instead

--- cpu/views.py
+++ cpu/views.py
@@ -7,13 +7,26 @@ from django.http import HttpResponse
 import random
 import math
 import sys
+from decimal import *
 
 def simple(request):
     """
     generate simple cpu running load
     """
-    for i in range(128):
-        n = random.randint(0, sys.maxint)
-        n = math.sqrt(n)
+    a = Decimal(1.0)
+    b = Decimal(1.0/math.sqrt(2))
+    t = Decimal(1.0)/Decimal(4.0)
+    p = Decimal(1.0)
+
+    for i in range(1024):
+        at = Decimal((a+b)/2)
+        bt = Decimal(math.sqrt(a*b))
+        tt = Decimal(t - p*(a-at)**2)
+        pt = Decimal(2*p)
+
+        a = at;b = bt;t = tt;p = pt
+
+    my_pi = (a+b)**2/(4*t)
+    accuracy = 100*(Decimal(math.pi)-my_pi)/my_pi
 
     return HttpResponse('')
